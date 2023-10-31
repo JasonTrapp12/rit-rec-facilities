7f139a54ea2c4fb774d69b26a4a01a0d731897d9
JasonTrapp12 <jwt9552@g.rit.edu>
2023-10-30 21:17:21 -0400

    midterm project

---
+++ src/components/SliderData.js
@@ -0,0 +1,33 @@
+export const SliderData = [
+    {
+        image: "/Images/tenniscourts.jpg"
+    },
+    {
+        image: "/Images/turffield.jpg"
+    },
+    {
+        image: "/Images/archery.jpg"
+    },
+    {
+        image: "/Images/barn.jpg"
+    },
+    {
+        image: "/Images/dougmay.jpg"
+    },
+    {
+        image: "/Images/clark.jpg"
+    },
+    {
+        image: "/Images/golf.jpg"
+    },
+    {
+        image: "/Images/swim.jpg"
+    },
+    {
+        image: "/Images/trails.jpg"
+    },
+    {
+        image: "/Images/wiedman.jpg"
+    },
+];
+
